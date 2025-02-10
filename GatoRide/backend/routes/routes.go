647425ef5b47dfa883c4fe1731a82ee1b8d3383c
shahid <mohammad.shahid@iitgn.ac.in>
2025-02-09 20:10:05 -0500

    debugging

--- GatoRide/backend/routes/routes.go
+++ GatoRide/backend/routes/routes.go
@@ -10,7 +10,7 @@ func SetupRoutes() *mux.Router {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/signup", controllers.Signup).Methods("POST")
-	router.HandleFunc("/verify-login", controllers.Login).Methods("GET")
+	router.HandleFunc("/verify-login", controllers.VerifyEmail).Methods("GET")
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
 
 	return router
