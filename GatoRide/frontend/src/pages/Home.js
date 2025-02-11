dbb0eb2f7321ebbaff5b82e7d5746a1d6847ddbf
DhanushUF18 <dhanushk.gujjula@ufl.edu>
2025-02-11 05:57:25 +0530

    added backgrounds

--- GatoRide/frontend/src/pages/Home.js
+++ GatoRide/frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="home-container">
+    <div className="page-container">
       <div className="home-content">
         <h1>Welcome to GatoRides</h1>
         <p>Experience easy and reliable ride-sharing!</p>
