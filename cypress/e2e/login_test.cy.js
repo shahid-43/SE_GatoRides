92b84f7b1c8fb9f3cda268411117f314fa09e6a0
Sandeep-2412 <machasndp@gmail.com>
2025-02-28 14:46:22 -0500

    Login test for Cypress

--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -1,6 +1,6 @@
 describe('Login Page Test', () => {
     it('should allow a user to log in', () => {
-      cy.visit('/login'); // Visit the login page
+      cy.visit('http://localhost:3000/login'); // Visit the login page
       cy.get('input[name="email"]').type('testuser@example.com'); // Type email
       cy.get('input[name="password"]').type('password123'); // Type password
       cy.get('button[type="submit"]').click(); // Click login button
