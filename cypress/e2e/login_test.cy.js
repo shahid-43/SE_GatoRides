1ec8f43e2a9c8f3fb1a5581f0d18a16f8a566188
Sandeep-2412 <machasndp@gmail.com>
2025-03-31 19:35:21 -0400

    Updated email and password

--- cypress/e2e/login_test.cy.js
+++ cypress/e2e/login_test.cy.js
@@ -1,8 +1,8 @@
 describe('Login Page Test', () => {
     it('should allow a user to log in', () => {
       cy.visit('http://localhost:3000/login'); // Visit the login page
-      cy.get('input[name="email"]').type('testuser@example.com'); // Type email
-      cy.get('input[name="password"]').type('password123'); // Type password
+      cy.get('input[name="email"]').type('r60y739794@tidissajiiu.com'); // Type email
+      cy.get('input[name="password"]').type('asdf'); // Type password
       cy.get('button[type="submit"]').click(); // Click login button
     });
   });
